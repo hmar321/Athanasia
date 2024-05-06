2ff79d72218c226bd42a9604818265eb5ff140e3
hmar321 <hmauricio.ar@gmail.com>
2024-05-06 14:20:38 +0200

    asd

---
+++ Athanasia/Services/ServiceLogicApps.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Security.KeyVault.Secrets;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Athanasia.Services
+{
+    public class ServiceLogicApps
+    {
+        private MediaTypeWithQualityHeaderValue header;
+        private string urlLogicApp;
+
+        public ServiceLogicApps(SecretClient secretClient)
+        {
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+            KeyVaultSecret urlTriger = secretClient.GetSecret("UrlLogicApp");
+            this.urlLogicApp = urlTriger.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string asunto, string mensaje)
+        {
+            var model = new
+            {
+                email = email,
+                asunto = asunto,
+                mensaje = mensaje
+            };
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                string json = JsonConvert.SerializeObject(model);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(this.urlLogicApp, content);
+            }
+        }
+    }
+}
